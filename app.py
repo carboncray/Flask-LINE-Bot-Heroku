bb9ef4e426c637524476bf639ab72d861080df70
carboncray <110658414+carboncray@users.noreply.github.com>
2022-08-08 14:23:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage()
+    get_message = event.message.text
     line_bot_api.reply_message(event.reply_token,message)
 
 

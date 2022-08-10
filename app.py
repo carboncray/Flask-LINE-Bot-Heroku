7d95d0767796b4a48cfb57e6412994136fd567fe
carboncray <110658414+carboncray@users.noreply.github.com>
2022-08-10 10:29:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,5 @@ def handle_message(event):
      
     if event.message.text == "學院": reply = LocationSendMessage(title='台北市職能發展學院',address='台北市士林區士東路301號',latitude=25.1147168,longitude=121.5325171)
     if event.message.text == "訂餐": reply = TextSendMessage(text="https://docs.google.com/spreadsheets/d/1kzhcIPBUlz2_U34-KQzhPWio4IQC5HHwwXPMMdzIe30/edit#gid=0")
-    if event.message.text == "貼圖": reply = StickertSendMessage(package_id='11538',sticker_id='51626494')    
+    if event.message.text == "貼圖": reply = StickertSendMessage(package_id='446',sticker_id='1988')    
     line_bot_api.reply_message(event.reply_token, reply)
